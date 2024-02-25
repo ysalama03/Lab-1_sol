6823e2561264fedb66cde9eba84be1d21b3e7abd
ysalama03 <79933264+ysalama03@users.noreply.github.com>
2024-02-25 15:27:49 +0200

    Update employee.js

--- Lab#1/BE/controllers/employee.js
+++ Lab#1/BE/controllers/employee.js
@@ -1,3 +1,7 @@
+const express = require('express');
+const app = express();
+const port = 3000; 
+app.use(express.json());
 const employee = [
   { id: '1', name: 'Mohamed Sayed' },
 ];
@@ -8,8 +12,42 @@ exports.getEmployees = async (req, res, next) => {
 
 // TODO
 exports.deleteEmployee = async (req, res, next) => {
+  app.delete('/employee/:id', (req, res) =>{
+    const employeeId = req.params.id;
+
+    // Find the index of the employee with the specified ID
+    const index = employee.findIndex((employee) => employee.id === employeeId);
+  
+    if (index !== -1) {
+      // Remove the employee from the array
+      employees.splice(index, 1);
+    
+    const response = {
+      message: `Employee with ID ${employeeId} deleted successfully`
+    };
+  
+    res.status(200).json(response);
+}});
 };
 
 // TODO
 exports.createEmployee = async (req, res, next) => {
+  app.post('/employee', async (req, res, next) => {
+    try {
+      const name = req.body.name;
+      const id = req.body.id;
+      const response = {
+        message: 'Employee created successfully',
+        data: {
+          id: id,
+          name: name
+        }
+      };
+  
+      res.status(200).json(response);
+    } catch (error) {
+      console.error('Error:', error);
+      res.status(500).json({ error: 'An error occurred' });
+    }
+  });
 };
